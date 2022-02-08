df531dcc5f6e871619477a7b5f9a7c20e843f3b9
Daksh <dpatel132@hawk.iit.edu>
2022-02-08 15:57:12 -0600

    Initial Commit

---
+++ Shared/ShieldingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShieldingApp.swift
+//  Shared
+//
+//  Created by Daksh Patel on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ShieldingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
